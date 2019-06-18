35c7f33135490ba5cf1d4ce330c8a74a14a03546
Danniel Zelaya <dannielzelaya@gmail.com>
2019-06-18 14:56:02 -0500

    pseudocode complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,111 @@
 // set variables
-// create array showing options 
-//random # generated and shown in random number box
-    // variable[Math.floor(Math.random() * variable.lenght)];
-//user clicks gem and each gem worth x points
+// create array showing options (#s are from 19-120)
+//random # generated and shown in random number box (#numbertoMatch)
+//user clicks gem and each gem worth x points (between 1-12)- has to be randomized
 //points added together and shown in total section
 //
 //if above random #, add +1 to losses and reset random#
 //if total = random #, +1 to wins and reset random #
 
-var userChoices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-var wins = 0;
-var losses = 0;
-var result ;
-var computerGuess = [];
-var computerChoices = ["19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40"]
+// Code to randomly generate # (between 19-120)  
+$(document).ready(function() {  
+    var random = Math.floor(Math.random() * 101 + 19);
 
+//assign(append) to #random-number div 
+    $('#random-number').text(random);
 
-document.onkeyup = function(event) {
-    userKeypressed = event.key;
-    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+// Assign each gem a random point value between 1-12 //
+    var gem1 = Math.floor(Math.random() * 11 + 1);
+    var gem2 = Math.floor(Math.random() * 11 + 1);
+    var gem3 = Math.floor(Math.random() * 11 + 1);
+    var gem4 = Math.floor(Math.random() * 11 + 1);
+
+// Declare variables for score keeping purposes
+    var userTotal = 0;
+    var wins = 0;
+    var losses = 0;
+
+//resets the game when a loss or win occurs
+$('#wins').text(wins);
+$('#losses').text(losses);
+
+function reset () {
+    random = Math.floor(Math.random() * 101 + 19)
+    console.log(random)
+    $('#random-number').text(random);
+    var gem1 = Math.floor(Math.random() * 11 + 1);
+    var gem2 = Math.floor(Math.random() * 11 + 1);
+    var gem3 = Math.floor(Math.random() * 11 + 1);
+    var gem4 = Math.floor(Math.random() * 11 + 1);
+    userTotal = 0;
+    $('#currentScore').text(userTotal);
 }
+
+// add win to userTotal
+    function victory() {
+        text('You won!');
+        wins++;
+        $('#wins').text(wins);
+        reset()
+    }
+
+// add loss to userTotal
+    function loser() {
+        text('You lost!');
+        losses++;
+        $('#losses').text(losses);
+        reset()
+    }
+
+// each gem has certain # of random points assigned to them when clicked//
+    $("g1").on('click', function() {
+        userTotal = userTotal + gem1;
+        console.log("New userTotal =" + userTotal);
+    $('currentscore').text(userTotal);
+
+// win/lose conditions
+        if (userTotal === random) {
+            victory();
+        }
+        else if (userTotal > random) {
+            loser();
+        }
+    })
+
+    $("g2").on('click', function() {
+        userTotal = userTotal + gem2;
+        console.log("New userTotal =" + userTotal);
+    $('currentscore').text(userTotal);
+        if (userTotal === random) {
+            victory();
+        }
+        else if (userTotal > random) {
+            loser();
+        }
+    })
+
+    $("g3").on('click', function() {
+        userTotal = userTotal + gem3;
+        console.log("New userTotal =" + userTotal);
+    $('currentscore').text(userTotal);
+        if (userTotal === random) {
+            victory();
+        }
+        else if (userTotal > random) {
+            loser();
+        }
+    })
+
+    $("g4").on('click', function() {
+        userTotal = userTotal + gem4;
+        console.log("New userTotal =" + userTotal);
+    $('currentscore').text(userTotal);
+        if (userTotal === random) {
+            victory();
+        }
+        else if (userTotal > random) {
+            loser();
+        }
+    });
+});
