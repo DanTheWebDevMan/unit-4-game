fcb5443f3a0a6b7c633aa5e68435268525cc5d1b
Danniel Zelaya <dannielzelaya@gmail.com>
2019-06-18 19:07:08 -0500

    have code put but no random # displays or buttons work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,111 +1,69 @@
-// set variables
-// create array showing options (#s are from 19-120)
-//random # generated and shown in random number box (#numbertoMatch)
-//user clicks gem and each gem worth x points (between 1-12)- has to be randomized
-//points added together and shown in total section
-//
-//if above random #, add +1 to losses and reset random#
-//if total = random #, +1 to wins and reset random #
+/*Psuedocoding for brain storming process
+set variables
+create array showing options (#s are from 19-120)
+random # generated and shown in random number box (#numbertoMatch)
+user clicks gem and each gem worth x points (between 1-12)- has to be randomized
+points added together and shown in total section
+if total > random #, add +1 to losses and reset random#
+if total = random #, +1 to wins and reset random # */
 
+$(document.ready(function(){
+// define global variables //
+var currentScore = 0;
+var winCounter = 0;
+var lossCounter = 0;
+var targetScore = 0;
+var crystalBtn = $('#crystalButton');
 
-// Code to randomly generate # (between 19-120)  
-$(document).ready(function() {  
-    var random = Math.floor(Math.random() * 101 + 19);
+// game function
+function startGame (){
+    currentScore = 0;
+    $("#yourScore").text(currentScore);
 
-//assign(append) to #random-number div 
-    $('#random-number').text(random);
+    $('#wins').text(winCounter);
+    $('#losses').text(lossCounter);
 
-// Assign each gem a random point value between 1-12 //
-    var gem1 = Math.floor(Math.random() * 11 + 1);
-    var gem2 = Math.floor(Math.random() * 11 + 1);
-    var gem3 = Math.floor(Math.random() * 11 + 1);
-    var gem4 = Math.floor(Math.random() * 11 + 1);
+//generate random target score (b/w 19-120)
+    var targetScore = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+        console.log ("target score is", targetScore);
+    $('#random-number').text(targetScore);
 
-// Declare variables for score keeping purposes
-    var userTotal = 0;
-    var wins = 0;
-    var losses = 0;
+// Have array choose 4 #s between 1 and 12
+var buttonValues = [];
+for (var i = 0; i < 4; i++) {
+    buttonValues.push(Math.round(Math.random() * 12))
+        console.log(buttonValues);
+    }
+//have random #s assigned to each gem
+    $('#button1').attr('value', buttonValues[0]);
+    $('#button2').attr('value', buttonValues[1]);
+    $('#button3').attr('value', buttonValues[2]);
+    $('#button4').attr('value', buttonValues[3]);
+}
+startGame();
 
-//resets the game when a loss or win occurs
-$('#wins').text(wins);
-$('#losses').text(losses);
+// game function
 
-function reset () {
-    random = Math.floor(Math.random() * 101 + 19)
-    console.log(random)
-    $('#random-number').text(random);
-    var gem1 = Math.floor(Math.random() * 11 + 1);
-    var gem2 = Math.floor(Math.random() * 11 + 1);
-    var gem3 = Math.floor(Math.random() * 11 + 1);
-    var gem4 = Math.floor(Math.random() * 11 + 1);
-    userTotal = 0;
-    $('#currentScore').text(userTotal);
+var playgame = function() {
+
+    currentScore = currentScore + (Number($(this).attr('value')));
+    $('#yourScore').text(currentScore)
+
+    if (currentScore === targetScore){
+        $('#currentScore').text(currentScore);
+        winCounter++;
+        $('#wins').text(winCounter);
+    }
+    else if (currentScore > targetScore){
+        $("#yourScore").text(currentScore);
+        lossCounter++;
+        $("#losses").text(lossCounter);
+    }
+    else {
+        //reset
+    }
 }
 
-// add win to userTotal
-    function victory() {
-        text('You won!');
-        wins++;
-        $('#wins').text(wins);
-        reset()
-    }
-
-// add loss to userTotal
-    function loser() {
-        text('You lost!');
-        losses++;
-        $('#losses').text(losses);
-        reset()
-    }
-
-// each gem has certain # of random points assigned to them when clicked//
-    $("g1").on('click', function() {
-        userTotal = userTotal + gem1;
-        console.log("New userTotal =" + userTotal);
-    $('currentscore').text(userTotal);
-
-// win/lose conditions
-        if (userTotal === random) {
-            victory();
-        }
-        else if (userTotal > random) {
-            loser();
-        }
-    })
-
-    $("g2").on('click', function() {
-        userTotal = userTotal + gem2;
-        console.log("New userTotal =" + userTotal);
-    $('currentscore').text(userTotal);
-        if (userTotal === random) {
-            victory();
-        }
-        else if (userTotal > random) {
-            loser();
-        }
-    })
-
-    $("g3").on('click', function() {
-        userTotal = userTotal + gem3;
-        console.log("New userTotal =" + userTotal);
-    $('currentscore').text(userTotal);
-        if (userTotal === random) {
-            victory();
-        }
-        else if (userTotal > random) {
-            loser();
-        }
-    })
-
-    $("g4").on('click', function() {
-        userTotal = userTotal + gem4;
-        console.log("New userTotal =" + userTotal);
-    $('currentscore').text(userTotal);
-        if (userTotal === random) {
-            victory();
-        }
-        else if (userTotal > random) {
-            loser();
-        }
-    });
-});
+// event listener for each clicked button
+    $('.crystalButton').on('click', playgame);
+}
